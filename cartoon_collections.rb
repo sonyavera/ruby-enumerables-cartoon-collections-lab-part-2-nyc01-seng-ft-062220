0b20ed200f2bc20767144e5e0111f41c14fe041b
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 22:47:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,9 @@ def long_planeteer_calls(planeteer_calls)
     element = element.length
     if element.any? > 4
       return true
-    end
-    if element.all? < 4
-      return false
+    # end
+    # if element.all? < 4
+    #   return false
   end
 end
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
