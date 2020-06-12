ce2f4c7d618cbd927efbdb2ef34f6881ddb070ab
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 22:46:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.collect do |element|
+  planeteer_calls.each do |element|
     element = element.length
     if element.any? > 4
       return true
